bd9f8b9f5816c718a151a8f4dba8bcbd9200dda1
MykhailoOtmakhov <65648953+MykhailoOtmakhov@users.noreply.github.com>
2020-10-19 09:25:28 +0300

    закрытие по клику на esk и overlay

--- js/gallery-code.js
+++ js/gallery-code.js
@@ -39,11 +39,55 @@ return evt.target.dataset.source;
 
 const lightboxClose = document.querySelector('.lightbox__button')
 lightboxClose.addEventListener('click', closeModal)
-
 function closeModal (evt) {
   if (evt.target.nodeName === 'BUTTON') {
     lightboxEl.classList.remove('is-open');
   } 
-} 
-// console.log(evt.target)
+ } 
+
+ const lightboxCloseOverlay = document.querySelector('.lightbox__overlay')
+ lightboxCloseOverlay.addEventListener('click', closeModalbyClick)
+ function closeModalbyClick (evt){
+ if (evt.target === evt.currentTarget){
+  lightboxEl.classList.remove('is-open');
+  }
+ }  
+
+ const lightboxCloseEsc = document.querySelector('.lightbox__content')
+ window.addEventListener('keydown', closeModalbyEsc)
+ function closeModalbyEsc (evt){
+  if (evt.code === "Escape"){
+  lightboxEl.classList.remove('is-open');}
+ }
+
+//  const UrlsArr = images.map({original} => original);
+//  const bigImageEl = document.querySelector('.lightbox__image')
+//  console.log(bigImageEl)
+   
+ 
+ // function keyboardPress(event) {
+//   if (event.code === "ArrowRight") {
+//     for (let i = 0; i < UrlsArr.length; i += 1) {
+//       if (bigImageEl.src === UrlsArr[8]) {
+//         bigImageEl.src = `${UrlsArr[0]}`;
+//         return;
+//       } else if (bigImageEl.src === UrlsArr[i]) {
+//         bigImageEl.src = `${UrlsArr[i + 1]}`;
+//         return;
+//       }
+//     }
+//   } else if (event.code === "ArrowLeft") {
+//     for (let i = 0; i < UrlsArr.length; i += 1) {
+//       if (bigImageEl.src === UrlsArr[0]) {
+//         bigImageEl.src = `${UrlsArr[8]}`;
+//         return;
+//       } else if (bigImageEl.src === UrlsArr[i]) {
+//         bigImageEl.src = `${UrlsArr[i - 1]}`;
+//         return;
+//       }
+//     }
+//   }
+// }
+// // const 
+// // console.log(evt.target)
   
