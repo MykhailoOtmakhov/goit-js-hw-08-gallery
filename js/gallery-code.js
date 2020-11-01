188214fefb7fe70055fb76aa97889cc1bef93060
MykhailoOtmakhov <65648953+MykhailoOtmakhov@users.noreply.github.com>
2020-11-01 10:41:29 +0200

    Доработал условия закрытия

--- js/gallery-code.js
+++ js/gallery-code.js
@@ -1,9 +1,5 @@
 import images from './gallery-items.js';
-console.log(images)
-
 const galleryContainer = document.querySelector('.js-gallery');
-
-// const galleryList = galleryContainer;
 const reducedString = images.reduce(
   (acc,{preview, original, description})=>{
     acc += 
@@ -25,69 +21,34 @@ const reducedString = images.reduce(
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 const lightboxEl = document.querySelector('.lightbox')
 const lightboxImageEl = document.querySelector('.lightbox__image')
+
 function onGalleryContainerClick(evt) { 
   evt.preventDefault()
 if (evt.target.nodeName !== 'IMG') {
   return;
 } 
-
 lightboxEl.classList.add('is-open');
 lightboxImageEl.src = evt.target.dataset.source;
 lightboxImageEl.alt = evt.target.alt;
-return evt.target.dataset.source;
 }
 
-const lightboxClose = document.querySelector('.lightbox__button')
+const lightboxClose = document.querySelector('.lightbox')
 lightboxClose.addEventListener('click', closeModal)
+
 function closeModal (evt) {
-  if (evt.target.nodeName === 'BUTTON') {
+  if (evt.target === document.querySelector('.lightbox__overlay') ||evt.target === document.querySelector('.lightbox__button')) {
     lightboxEl.classList.remove('is-open');
-  } 
- } 
-
- const lightboxCloseOverlay = document.querySelector('.lightbox__overlay')
- lightboxCloseOverlay.addEventListener('click', closeModalbyClick)
- function closeModalbyClick (evt){
- if (evt.target === evt.currentTarget){
-  lightboxEl.classList.remove('is-open');
+    lightboxImageEl.src = ' ';
+    lightboxImageEl.alt = ' ';
   }
- }  
+ }
 
- const lightboxCloseEsc = document.querySelector('.lightbox__content')
  window.addEventListener('keydown', closeModalbyEsc)
  function closeModalbyEsc (evt){
   if (evt.code === "Escape"){
-  lightboxEl.classList.remove('is-open');}
+  lightboxEl.classList.remove('is-open');
+  lightboxImageEl.src = ' ';
+  lightboxImageEl.alt = ' ';
+  }
  }
 
-//  const UrlsArr = images.map({original} => original);
-//  const bigImageEl = document.querySelector('.lightbox__image')
-//  console.log(bigImageEl)
-   
- 
- // function keyboardPress(event) {
-//   if (event.code === "ArrowRight") {
-//     for (let i = 0; i < UrlsArr.length; i += 1) {
-//       if (bigImageEl.src === UrlsArr[8]) {
-//         bigImageEl.src = `${UrlsArr[0]}`;
-//         return;
-//       } else if (bigImageEl.src === UrlsArr[i]) {
-//         bigImageEl.src = `${UrlsArr[i + 1]}`;
-//         return;
-//       }
-//     }
-//   } else if (event.code === "ArrowLeft") {
-//     for (let i = 0; i < UrlsArr.length; i += 1) {
-//       if (bigImageEl.src === UrlsArr[0]) {
-//         bigImageEl.src = `${UrlsArr[8]}`;
-//         return;
-//       } else if (bigImageEl.src === UrlsArr[i]) {
-//         bigImageEl.src = `${UrlsArr[i - 1]}`;
-//         return;
-//       }
-//     }
-//   }
-// }
-// // const 
-// // console.log(evt.target)
-  
